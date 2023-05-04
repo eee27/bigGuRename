0e2f442a4bef8f99d4afad1f766560d9db77c565
liu bo <eee27go@outlook.com>
2023-05-04 11:50:35 +0800

    deving

--- bigGuRename/MainPage.xaml.cs
+++ bigGuRename/MainPage.xaml.cs
@@ -11,6 +11,7 @@ public partial class MainPage : ContentPage
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
+		/*
 		count++;
 
 		if (count == 1)
@@ -19,6 +20,58 @@ public partial class MainPage : ContentPage
 			CounterBtn.Text = $"Clicked {count} times";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
+		*/
 	}
+
+	private void OnTargetClicked(object sender, EventArgs e)
+	{
+		string originDateStr = originDate.Text;
+		string targetStartDateStr=targetStartDate.Text;
+		string targetEndDateStr=targetEndDate.Text;
+
+		if (!string.IsNullOrEmpty(originDateStr)
+			&& !string.IsNullOrEmpty(targetStartDateStr)
+			&& !string.IsNullOrEmpty(targetEndDateStr)
+
+			&& originDateStr.All(char.IsDigit)
+			&& targetStartDateStr.All(char.IsDigit)
+			&& targetEndDateStr.All(char.IsDigit)
+			) 
+		{
+
+
+		}
+		else
+		{
+			DisplayAlert("请检查日期格式", "日期格式有误,请确定三个日期均为纯数字且非空", "关闭");
+		}
+
+
+		
+	}
+
+	private void OnNumTextChanged(object sender, TextChangedEventArgs e)
+	{
+		/*
+		string oldText = e.OldTextValue;
+		string newText = e.NewTextValue;
+
+		
+
+		if(newText.All(char
+			.IsDigit))
+		{
+			
+		}
+		else
+		{
+			
+
+		}
+		*/
+	   
+	}
+
+
 }
 
